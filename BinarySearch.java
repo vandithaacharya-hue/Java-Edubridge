3649108e6518829e6b20d288baa1314f2d6d03ee
vandithaacharya-hue <vandithaacharya@gmail.com>
2026-09-21 18:36:24 +0530

    Create BinarySearch.java
    Added a Java program to search for an element in a sorted array using binary search

---
+++ BinarySearch.java
@@ -0,0 +1,38 @@
+public class BinarySearch {
+
+    static int search(int[] arr, int target) {
+
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 5, 8, 12, 16, 23, 38};
+        int target = 23;
+
+        int result = search(arr, target);
+
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+    }
+}
